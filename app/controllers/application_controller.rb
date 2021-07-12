93bb773d89da539573528b5f66ae17b6b7333336
konoha1230 <tabino8_hajimari@yahoo.co.jp>
2021-07-12 16:34:09 +0900

    deviseの導入とそれに伴い必要となったCSSファイルの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameter, if: :devise_controller?
+
+  private
+  def configure_permitted_parameter
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
+  end
 end
