c5639544d85b28ceef797e9dd73aaeca80423f33
konoha1230 <tabino8_hajimari@yahoo.co.jp>
2021-07-15 13:15:36 +0900

    fakerで漢字名、フリガナの指定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,16 @@
 FactoryBot.define do
   factory :user do
+    transient do
+      person {Gimei.name}
+    end
     nickname              {Faker::Name.name}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {"a6" + Faker::Internet.password(min_length: 4)}
     password_confirmation {password}
-    last_name             {Faker::Name.last_name}
-    first_name            {Faker::Name.first_name}
-    last_name_kana        {last_name}
-    first_name_kana       {first_name}
+    last_name             {person.last.kanji}
+    first_name            {person.first.kanji}
+    last_name_kana        {person.last.katakana}
+    first_name_kana       {person.first.katakana}
     birthday              {Faker::Date.birthday(min_age: 18)}
   end
 end
