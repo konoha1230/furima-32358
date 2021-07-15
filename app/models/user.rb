afc89b541813da50172589febfb3b1eef42162f1
konoha1230 <tabino8_hajimari@yahoo.co.jp>
2021-07-16 08:30:07 +0900

    userモデルのリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "は漢字、全角ひらがな、全角カタカナで入力してください" }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "は漢字、全角ひらがな、全角カタカナで入力してください" }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい' }
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname, :birthday
+    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "は漢字、全角ひらがな、全角カタカナで入力してください" }
+    validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい' }
+  end
 
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,100}\z/i, message: "は英数字混同で入力してください" }
 end
