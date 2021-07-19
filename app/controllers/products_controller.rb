89a06222b55638adc3e3f0e27d0570ab0c002c01
konoha1230 <tabino8_hajimari@yahoo.co.jp>
2021-07-19 18:32:21 +0900

    newアクションとcreateアクションの記述

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,21 @@
 class ProductsController < ApplicationController
 
   def new
+    @product = Product.new
   end
 
   def create
+    if @product.save(product_params)
+      redirect to: root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :content, :price, :status_id, :category_id, :delivery_charge_id, :prefecture_id, :duration_id).merge(user_id: current_user.id)
   end
   
 end
