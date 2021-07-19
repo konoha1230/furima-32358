28836d6b7bed67c07c3c0f1cb7bce5eafcb14728
konoha1230 <tabino8_hajimari@yahoo.co.jp>
2021-07-19 14:53:19 +0900

    バリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  with_options presence: true do
+    validates :name, :content, :price, :status_id, :delivery_charge_id, :category_id, :prefecture_id, :duration_id, :user
+  end
 end
