d1017ad62f8689d9b60c4860d43c21974c56c18d
konoha1230 <tabino8_hajimari@yahoo.co.jp>
2021-07-19 18:04:49 +0900

    ActiveHashの導入

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,19 @@
 class Product < ApplicationRecord
   with_options presence: true do
-    validates :name, :content, :price, :status_id, :delivery_charge_id, :category_id, :prefecture_id, :duration_id, :user
+    validates :name, :content, :price, :user
+  end
+
+  with_options numericality: { other_then: 1 , message: "can't be blank" } do
+    validates :status_id, :delivery_charge_id, :category_id, :prefecture_id, :duration_id
   end
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :duration
 end
