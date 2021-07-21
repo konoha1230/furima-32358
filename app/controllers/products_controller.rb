6a4d6bd7107b40057e6a5a0cbb2f00725e6fdce0
konoha1230 <tabino8_hajimari@yahoo.co.jp>
2021-07-21 10:05:38 +0900

    imageカラムの保存を許可する記述

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,7 +19,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :content, :price, :status_id, :category_id, :delivery_charge_id, :prefecture_id, :duration_id).merge(user_id: current_user.id)
+    params.require(:product).permit(:image, :name, :content, :price, :status_id, :category_id, :delivery_charge_id, :prefecture_id, :duration_id).merge(user_id: current_user.id)
   end
   
 end
