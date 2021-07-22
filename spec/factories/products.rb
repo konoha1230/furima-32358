b31d2f7daa961c0b43e479795dfe107e91a59bc9
konoha1230 <tabino8_hajimari@yahoo.co.jp>
2021-07-22 11:47:32 +0900

    テストのダミーデータ作成

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :product do
-    
+    name {Faker::Lorem.word}
+    content {Faker::Lorem.sentence}
+    category_id {rand(2..11)}
+    status_id {rand(2..7)}
+    delivery_charge_id {rand(2..3)}
+    prefecture_id {rand(2..48)}
+    duration_id {rand(2..4)}
+    association :user
+
+    after(:build) do |product|
+      product.image.attach(io: File.open('public/images/mobile-battery.JPG'), filename: 'mobile-battery.JPG')
+    end
   end
 end
