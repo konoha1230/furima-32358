b3e6973634d261f07d065064ecf8ff60fa017b0d
konoha1230 <tabino8_hajimari@yahoo.co.jp>
2021-07-23 20:19:37 +0900

    テストコードの記述

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :product do
     name {Faker::Lorem.word}
     content {Faker::Lorem.sentence}
+    price {rand(300..9999999)}
     category_id {rand(2..11)}
     status_id {rand(2..7)}
     delivery_charge_id {rand(2..3)}
