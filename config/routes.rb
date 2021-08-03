d1fec9f0963e4520d1b8aea7c8502cd6ab3b7700
konoha1230 <tabino8_hajimari@yahoo.co.jp>
2021-08-03 15:57:16 +0900

    ルーティングのネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
-  resources :products, only: [:new, :create, :show, :edit, :update, :destroy]
-  resources :orders, only: [:index, :create]
+  resources :products, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :orders, only: [:index, :create]
+  end
 end
