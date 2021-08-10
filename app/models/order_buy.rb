4fd31ae57952ab329eb111cb9d7b3b6f5bea1559
konoha1230 <tabino8_hajimari@yahoo.co.jp>
2021-08-11 08:33:10 +0900

    バリデーションの設定

--- app/models/order_buy.rb
+++ app/models/order_buy.rb
@@ -3,9 +3,10 @@ class OrderBuy
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :telephone
 
   with_options presence: true do
-    validates :postal_code, :city, :address
+    validates :city, :address
+    validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/, message: "はハイフンを入れて７桁の数値で入力してください"}
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
-    validates :telephone, numericality: { only_integer: true, message: "is invalid. Input half-width characters." }
+    validates :telephone, format: { with: /\A\d{10,11}\z/, message: "はハイフンなしで10、11桁で入力してください" }
   end
 
   def save
