075cd3bf46788d8995f07b5d68033265e1c26835
konoha1230 <tabino8_hajimari@yahoo.co.jp>
2021-08-14 08:00:18 +0900

    トークンのバリデーションの設定

--- app/models/order_buy.rb
+++ app/models/order_buy.rb
@@ -3,9 +3,9 @@ class OrderBuy
   attr_accessor :token, :postal_code, :prefecture_id, :city, :address, :building, :telephone
 
   with_options presence: true do
-    validates :city, :address
-    validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/, message: "はハイフンを入れて７桁の数値で入力してください"}
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
+    validates :city, :address, :token
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "はハイフンを入れて７桁の数値で入力してください" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :telephone, format: { with: /\A\d{10,11}\z/, message: "はハイフンなしで10、11桁で入力してください" }
   end
 
