ab381b8bc1a9cb7bef3593455d773836ef2faf87
konoha1230 <tabino8_hajimari@yahoo.co.jp>
2021-08-15 20:36:33 +0900

    単体テストの実装

--- app/models/order_buy.rb
+++ app/models/order_buy.rb
@@ -4,9 +4,9 @@ class OrderBuy
 
   with_options presence: true do
     validates :city, :address, :token
-    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "はハイフンを入れて７桁の数値で入力してください" }
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "はハイフンを入れて７桁の半角数字で入力してください" }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :telephone, format: { with: /\A\d{10,11}\z/, message: "はハイフンなしで10、11桁で入力してください" }
+    validates :telephone, format: { with: /\A\d{10,11}\z/, message: "はハイフンなしで10、11桁の半角数字で入力してください" }
   end
 
   def save
