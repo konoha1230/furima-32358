4af79a94101ceb2e509ba146fc3d469db41d274a
konoha1230 <tabino8_hajimari@yahoo.co.jp>
2021-08-19 20:27:05 +0900

    購入後の表示とリンク先の設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,9 +2,13 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :get_data, only: [:show, :edit, :update, :destroy]
   before_action :user_judge, only: [:edit, :update, :destroy]
+  before_action :get_data_order, only: [:index, :show, :edit, :destroy]
 
   def index
     @products = Product.all.order('created_at DESC')
+    @products.each do |product|
+      order_counter(@orders, product)
+    end
   end
 
   def new
@@ -21,9 +25,14 @@ class ProductsController < ApplicationController
   end
 
   def show
+    order_counter(@orders, @product)
   end
 
   def edit
+    order_counter(@orders, @product)
+    if @count == 1
+      redirect_to root_path
+    end
   end
 
   def update
@@ -35,7 +44,10 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @product.destroy
+    order_counter(@orders, @product)
+    if @count == 0
+      @product.destroy
+    end
     redirect_to root_path
   end
 
@@ -48,6 +60,20 @@ class ProductsController < ApplicationController
 
   def get_data
     @product = Product.find(params[:id])
+  end
+
+  def get_data_order
+    @orders = Order.includes(:product)
+  end
+
+  def order_counter(orders, product)
+    @count = 0
+    orders.each do |order|
+      if order.product_id == product.id
+        @count = 1
+        return
+      end
+    end
   end
 
   def user_judge
