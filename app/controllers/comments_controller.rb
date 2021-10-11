cf0e25047dfe668ab3d772fc8221932b45edc2a0
konoha1230 <tabino8_hajimari@yahoo.co.jp>
2021-10-11 11:26:05 +0900

    商品詳細ページのコメント入力フォームの編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,10 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.create(comment_params)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, product: params[:product_id])
   end
 end
--- db/migrate/20211007041722_create_comments.rb
+++ db/migrate/20211007041722_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.string :content, null: false
+      t.string :text, null: false
       t.references :user, null: false, foreign_key: true
       t.references :product, null: false, foreign_key: true
       t.timestamps
